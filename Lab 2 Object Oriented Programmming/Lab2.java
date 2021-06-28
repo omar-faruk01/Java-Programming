fb04e3b2f90f04cd10d3fad378386d7c1d2c1878
Omar Faruk <70847377+omar-faruk01@users.noreply.github.com>
2021-06-28 09:24:20 -0400

    OOP
    Creating a triangle class to create objects and carry out operations to get perimeter and area.

--- Lab 2 Object Oriented Programmming/Lab2.java
+++ Lab 2 Object Oriented Programmming/Lab2.java
@@ -1,1 +1,43 @@
+package com.company;
 
+
+class Triangle{
+    int x;
+    int y;
+    int z;
+
+    Triangle(int a, int b, int c){
+        x = a;
+        y = b;
+        z = c;
+    };
+
+    double getArea(){
+        int p;
+        int q;
+        p = (x + y + z)/2;
+        q = (p*(p-x)*(p-y)*(p-z));
+        return Math.sqrt(q);
+    }
+
+    int getCircleLength()
+    {
+        int p;
+        return p = (x + y + z);
+    };
+}
+
+public class Lab2 {
+
+    public static void main(String[] args)
+    {
+        Triangle desk = new Triangle(3,4,5);
+        Triangle table = new Triangle(7,8,9);
+
+        System.out.println("Triangle 1   Area: " + desk.getArea() + " Circle Length " + desk.getCircleLength());
+        System.out.println("Triangle 2   Area: " + table.getArea() + " Circle Length " + table.getCircleLength());
+
+
+    }
+
+}
