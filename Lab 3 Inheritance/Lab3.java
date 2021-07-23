c10685c0a3d03e1f6ab2350d3ad2727f2c61b729
Omar Faruk <70847377+omar-faruk01@users.noreply.github.com>
2021-07-23 13:13:50 -0400

    Inheritance
    Creating a Person class, including name, gender, age etc. Inherit the Person class to design the Teacher class, add title, department and other fields. Inheriting the Person class to design Student class, add student ID, admission time, major and other fields. Define all classes’ constructor and toString() methods, and testing them with the following objects:-Student no1-Teacher t1-Print the information of objects

--- Lab 3 Inheritance/Lab3.java
+++ Lab 3 Inheritance/Lab3.java
@@ -1,1 +1,78 @@
+package com.company;
+import java.util.*;
+
+public class Lab3 {
+
+    public static void main(String[] args)
+    {
+        Student no1 = new Student("Omar", "male", 20, "IT", "Senior",1);
+        Teacher t1 = new Teacher("Farooq", "male", 56, "Judge", "Law", 120000);
+        printToString(no1);
+        printToString(t1);
+    }
+
+    public static void printToString(Object x)
+    {
+        System.out.println(x.toString());
+    }
+}
+
+class Student extends Person{
+    private int studentID;
+    private java.util.Date admissionTime;
+    private String major;
+    private String classStanding;
+
+    public Student( String nme, String g, int num, String spec, String standing, int id){
+        super(nme, g, num);
+        major = spec;
+        admissionTime = new java.util.Date();
+        classStanding = standing;
+        studentID = id;
+    }
+
+    public String toString()
+    {
+        return "Student, " + name + " - Student ID: " + studentID + ", is majoring in  "
+                + major + " as a " + classStanding + " and was first admitted on \n" + admissionTime + ". "
+                + name + " is a " + gender + " and " + age + " years old.";
+    }
+}
+
+class Teacher extends Person {
+    private String title;
+    private String department;
+    private int salary;
+
+    public Teacher( String nme, String g, int num, String ttl, String dept, int sal){
+        super(nme, g, num);
+        title = ttl;
+        department = dept;
+        salary = sal;
+    }
+
+    public String toString()
+    {
+        return name + " is a " + title + " teaching in " + department+ " and getting paid $" + salary + ".\n" +
+        name + " is a " + gender + " and " + age + " years old.";
+    }
+}
+
+class Person {
+    public String name;
+    public String gender;
+    public int age;
+
+    public Person(String nme, String g, int num){
+        name = nme;
+        gender = g;
+        age = num;
+    }
+
+    public String toString()
+    {
+        return name + " is a " + gender + " and " + age + " years old.";
+    }
+}
+
 
